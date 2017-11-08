c8f46a0e6736aa39aa3a41e4e19ad8a09f374c78
chao <472070381@qq.com>
2017-11-08 17:36:14 +0800

    mavenProject

---
+++ onlineTestSystem/src/cn/gdufs/maven/Hello.java
@@ -0,0 +1,8 @@
+package cn.gdufs.maven;
+
+public class Hello {
+   public void Hello()
+   {
+	   System.out.println("hello,world!");
+   }
+}
---
+++ onlineTestSystem/src/cn/gdufs/maven/HelloTest.java
@@ -0,0 +1,12 @@
+package cn.gdufs.maven;
+
+import org.junit.Test;
+
+public class HelloTest {
+	@Test
+   public void testHello()
+   {
+	   Hello hello = new Hello();
+	   hello.Hello();
+   }
+}
